b887af641f215502aa8664a3e440341af4694827
ngoctb13 <ngoctb136@gmail.com>
2023-06-23 03:36:50 +0700

    update-create-leauge-servlet

--- src/java/controller/LeaugeCreateServlet.java
+++ src/java/controller/LeaugeCreateServlet.java
@@ -5,19 +5,25 @@
 package controller;
 
 import dao.TourDAO;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 import model.Tour;
 import model.User;
 
@@ -25,6 +31,9 @@ import model.User;
  *
  * @author Admin
  */
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2,
+        maxFileSize = 1024 * 1024 * 10,
+        maxRequestSize = 1024 * 1024 * 50)
 public class LeaugeCreateServlet extends HttpServlet {
 
     /**
@@ -42,7 +51,24 @@ public class LeaugeCreateServlet extends HttpServlet {
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("user");
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-
+        //------------------------------------------------------------
+        Part part = request.getPart("avatar_leauge");
+        String fileName = extractFileName(part);
+        String savePath = "D:\\1_SWP391\\All for Java\\LeagueManagement\\web\\images" + File.separator + fileName;
+        File fileSaveDir = new File(savePath);
+        part.write(savePath + File.separator);
+//        Part file = request.getPart("avatar_leauge");
+//        String imageFileName = file.getSubmittedFileName();       
+//        String uploadPath = "D:/1_SWP391/All for Java/LeagueManagement/web/images" + imageFileName;
+//        
+//        FileOutputStream fos = new FileOutputStream(uploadPath);
+//        InputStream is = file.getInputStream();
+//        
+//        byte[] data = new byte[is.available()];
+//        is.read(data);
+//        fos.write(data);
+//        fos.close();
+        //---------------------------------------------------------
         String tour_name = request.getParameter("tour_name");
         String tour_phone = request.getParameter("tour_phone");
         int quantity = Integer.parseInt(request.getParameter("quantity"));
@@ -55,15 +81,16 @@ public class LeaugeCreateServlet extends HttpServlet {
         String end_date = request.getParameter("end_date");
 //        Date sDate = (Date) dateFormat.parse(start_date);
 //        Date eDate = (Date) dateFormat.parse(end_date);
-        Tour tour = new Tour(tour_name, address, tour_phone, start_date, end_date, description, host, type, quantity);
+        Tour t = new Tour(tour_name, address, fileName, tour_phone, start_date, end_date, description, host, type, quantity);
         TourDAO dao = new TourDAO();
 
-        int add = dao.addTour(tour);
-
+        int add = dao.addTour(t);
         if (add > 0) {
-            request.setAttribute("gotTour", tour);
+            request.setAttribute("gotTour", t);
             request.getRequestDispatcher("leaugeProfile.jsp").forward(request, response);
         }
+        
+               
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -114,5 +141,16 @@ public class LeaugeCreateServlet extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
+    private String extractFileName(Part part) {
+        String contentDisp = part.getHeader("content-disposition");
+        String[] items = contentDisp.split(";");
+        for (String s : items) {
+            if (s.trim().startsWith("filename")) {
+                return s.substring(s.indexOf("=") + 2, s.length() - 1);
+            }
+        }
+        return "";
+    }
 
 }
