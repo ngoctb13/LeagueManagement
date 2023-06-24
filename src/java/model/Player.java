c48382779aef5c22c084108cd0196e0eec13e6c5
Vu Minh Duc <ducvm9820@gmail.com>
2023-06-25 00:57:02 +0700

    Create Player.java

---
+++ src/java/model/Player.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Admin
+ */
+public class Player {
+    private int player_id;
+    private int user_id;
+    private int team_id;
+    private String position;
+    private int shirt_number;
+    private boolean isManager;
+
+    public Player() {
+    }
+
+    public Player(int player_id, int user_id, int team_id, String position, int shirt_number, boolean isManager) {
+        this.player_id = player_id;
+        this.user_id = user_id;
+        this.team_id = team_id;
+        this.position = position;
+        this.shirt_number = shirt_number;
+        this.isManager = isManager;
+    }
+
+    public int getPlayer_id() {
+        return player_id;
+    }
+
+    public void setPlayer_id(int player_id) {
+        this.player_id = player_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getTeam_id() {
+        return team_id;
+    }
+
+    public void setTeam_id(int team_id) {
+        this.team_id = team_id;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getShirt_number() {
+        return shirt_number;
+    }
+
+    public void setShirt_number(int shirt_number) {
+        this.shirt_number = shirt_number;
+    }
+
+    public boolean isIsManager() {
+        return isManager;
+    }
+
+    public void setIsManager(boolean isManager) {
+        this.isManager = isManager;
+    }
+    
+    
+}
