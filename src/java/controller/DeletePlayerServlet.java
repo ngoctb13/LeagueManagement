e41fb76725074d9945a20ae13fb4cf9b86cef769
Vu Minh Duc <ducvm9820@gmail.com>
2023-06-26 10:38:13 +0700

    update deletePlayer function

---
+++ src/java/controller/DeletePlayerServlet.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dao.PlayerDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Admin
+ */
+public class DeletePlayerServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, Exception {
+        response.setContentType("text/html;charset=UTF-8");
+        int team_id = Integer.parseInt(request.getParameter("team_id"));
+        int player_id = Integer.parseInt(request.getParameter("player_id"));
+        int user_id = Integer.parseInt(request.getParameter("user_id"));
+
+        try {
+            PlayerDAO playerDAO = new PlayerDAO();
+            if (playerDAO.IsTeamManager(user_id, team_id)) {
+                playerDAO.deletePlayer(player_id);
+            }
+            request.getRequestDispatcher("home.jsp").forward(request, response);
+
+        } catch (Exception ex) {
+            Logger.getLogger(ParticipantListServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(DeletePlayerServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(DeletePlayerServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- src/java/dao/PlayerDAO.java
+++ src/java/dao/PlayerDAO.java
@@ -30,7 +30,7 @@ public class PlayerDAO extends DBContext {
             ps.setInt(1, team_id);
             rs = ps.executeQuery();
             ArrayList<Player> list = new ArrayList<>();
-            while (rs.next()) {       
+            while (rs.next()) {
                 Player player = new Player();
                 player.setPlayer_id(rs.getInt("player_id"));
                 player.setUser_id(rs.getInt("user_id"));
@@ -41,7 +41,46 @@ public class PlayerDAO extends DBContext {
                 list.add(player);
             }
             return list;
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            closeResultSet(rs);
+            closePreparedStatement(ps);
+            closeConnection(con);
+        }
+    }
+    
+    public Boolean IsTeamManager (int user_id, int team_id ) throws Exception{
+         try {
+            String query = "SELECT * FROM player where user_id = ? AND team_id = ?";
+            con = getConnection();
+            ps = con.prepareStatement(query);
+            ps.setInt(1, user_id);
+            ps.setInt(2, team_id);
+            rs = ps.executeQuery();
             
+            if(rs.next()){
+                return rs.getBoolean("isManager");
+            }
+            return false;
+            
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            closeResultSet(rs);
+            closePreparedStatement(ps);
+            closeConnection(con);
+        }
+    }
+
+    public void deletePlayer(int player_id) throws Exception {
+        try {
+            String query = "DELETE FROM player where player_id = ?";
+            con = getConnection();
+            ps = con.prepareStatement(query);
+            ps.setInt(1, player_id);
+            rs = ps.executeQuery();
         } catch (Exception e) {
             throw e;
         } finally {
