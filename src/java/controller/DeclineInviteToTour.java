c7bdf74c2e66e7fe177ed232f860f5a0d5c5f860
sonnehihi <son8h123@gmail.com>
2023-07-14 20:52:48 +0700

    decline invite to tour servlet

--- src/java/controller/DeclineInviteToTour.java
+++ src/java/controller/DeclineInviteToTour.java
@@ -79,13 +79,7 @@ public class DeclineInviteToTour extends HttpServlet {
         }
     } 
 
-    /** 
-     * Handles the HTTP <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
