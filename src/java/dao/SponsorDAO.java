3dc2a41efd4db6e293e30b6214dd7931c3e4f7f3
thyur <133103963+thyur@users.noreply.github.com>
2023-07-14 23:18:04 +0700

    update sponsorDAO

--- src/java/dao/SponsorDAO.java
+++ src/java/dao/SponsorDAO.java
@@ -3,11 +3,97 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dao;
+import context.DBContext;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import model.Sponsor;
 
 /**
  *
  * @author HP
  */
-public class SponsorDAO {
-    
+public class SponsorDAO extends DBContext {
+
+    private Connection con;
+    private PreparedStatement ps;
+    private ResultSet rs;
+    public int addSponsor(Sponsor a) throws Exception {
+        int status = 0;
+        try {
+            con = getConnection();
+            ps = con.prepareStatement("insert into sponsor ( tour_id, image, link) values (?,?,?,?)");
+            ps.setInt(1, a.getTour_id());
+            ps.setString(2, a.getImage());
+            ps.setString(3, a.getLink());
+            status = ps.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            closeResultSet(rs);
+            closePreparedStatement(ps);
+            closeConnection(con);
+        }
+        return status;
+    }
+    public void deleteSponsor(int sponsor_id) throws Exception {
+        try {
+            String query = "DELETE FROM sponsor where sponsor_id = ?";
+            con = getConnection();
+            ps = con.prepareStatement(query);
+            ps.setInt(1, sponsor_id);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            closePreparedStatement(ps);
+            closeConnection(con);
+        }
+    }
+    public int updateSponsor(Sponsor a) throws Exception {
+        int status = 0;
+        try {
+            con = getConnection();
+            ps = con.prepareStatement("UPDATE `sponsor`SET `image` = ?, `link` = ?"
+                    + "WHERE `sponsor_id` = ?; ");
+            ps.setString(1, a.getImage());            
+            ps.setString(2, a.getLink());            
+                     
+            status = ps.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            closeResultSet(rs);
+            closePreparedStatement(ps);
+            closeConnection(con);
+        }
+        return status;
+    }
+    public List<Sponsor> getListSponsorByTourID(int input_id) throws Exception {
+        try {
+            String sql = "SELECT * FROM sponsor WHERE tour_id = ? ";
+            con = getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, input_id);
+            rs = ps.executeQuery();
+            List<Sponsor> list = new ArrayList<>();
+            while (rs.next()) {
+                int sponsor_id = rs.getInt("sponsor_id");
+                int tour_id = rs.getInt("tour_id");
+                String image = rs.getString("image");
+                String link = rs.getString("link");
+                Sponsor sponsor = new Sponsor(sponsor_id, tour_id, image, link);
+                list.add(sponsor);
+            }
+            return list;
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            closeResultSet(rs);
+            closePreparedStatement(ps);
+            closeConnection(con);
+        }
+    }
 }
--- src/java/model/Sponsor.java
+++ src/java/model/Sponsor.java
@@ -9,5 +9,60 @@ package model;
  * @author HP
  */
 public class Sponsor {
+    int sponsor_id;
+    int tour_id;
+    String image;
+    String link;
+
+    public Sponsor() {
+    }
+
+    public Sponsor(int sponsor_id, int tour_id, String image, String link) {
+        this.sponsor_id = sponsor_id;
+        this.tour_id = tour_id;
+        this.image = image;
+        this.link = link;
+    }
+    public Sponsor( int tour_id, String image, String link) {
+        this.tour_id = tour_id;
+        this.image = image;
+        this.link = link;
+    }
+
+    public int getSponsor_id() {
+        return sponsor_id;
+    }
+
+    public void setSponsor_id(int sponsor_id) {
+        this.sponsor_id = sponsor_id;
+    }
+
+    public int getTour_id() {
+        return tour_id;
+    }
+
+    public void setTour_id(int tour_id) {
+        this.tour_id = tour_id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    
+
+   
     
 }
