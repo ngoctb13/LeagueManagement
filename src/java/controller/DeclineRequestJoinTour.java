2130662aaeda0f735740553d04cd36adeb6fd764
sonnehihi <son8h123@gmail.com>
2023-07-14 20:41:29 +0700

    update respond join league

--- src/java/controller/DeclineRequestJoinTour.java
+++ src/java/controller/DeclineRequestJoinTour.java
@@ -31,19 +31,7 @@ public class DeclineRequestJoinTour extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet DeclineRequestJoinTour</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet DeclineRequestJoinTour at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
     } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -85,7 +73,7 @@ public class DeclineRequestJoinTour extends HttpServlet {
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException if an I/O error occurs 
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
